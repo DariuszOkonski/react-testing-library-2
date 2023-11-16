e3b8f14cced77e8430962b4d9b6f43115b01d0d5
Dariusz Okoński <darek200180@gmail.com>
2023-11-16 18:43:26 +0100

    start from scratch

--- src/UserForm.test.js
+++ src/UserForm.test.js
@@ -3,38 +3,40 @@ import user from '@testing-library/user-event';
 import UserForm from './UserForm';
 import { act } from 'react-dom/test-utils';
 
-test('it shows two inputs and a button', () => {
-  const mock = jest.fn();
-  render(<UserForm onUserAdd={mock} />);
+test('it shows two inputs and a button', () => {});
 
-  const inputs = screen.getAllByRole('textbox');
-  const button = screen.getByRole('button');
+// test('it shows two inputs and a button', () => {
+//   const mock = jest.fn();
+//   render(<UserForm onUserAdd={mock} />);
 
-  expect(inputs).toHaveLength(2);
-  expect(button).toBeInTheDocument();
-});
+//   const inputs = screen.getAllByRole('textbox');
+//   const button = screen.getByRole('button');
 
-test('it calls onUserAdd when the form is submitted (not the best implementation)', () => {
-  const argList = [];
-  const callback = (...args) => {
-    argList.push(args);
-  };
-  render(<UserForm onUserAdd={callback} />);
+//   expect(inputs).toHaveLength(2);
+//   expect(button).toBeInTheDocument();
+// });
 
-  const [nameInput, emailInput] = screen.getAllByRole('textbox');
-  const button = screen.getByRole('button');
+// test('it calls onUserAdd when the form is submitted (not the best implementation)', () => {
+//   const argList = [];
+//   const callback = (...args) => {
+//     argList.push(args);
+//   };
+//   render(<UserForm onUserAdd={callback} />);
 
-  // eslint-disable-next-line testing-library/no-unnecessary-act
-  act(() => {
-    user.click(nameInput);
-    user.keyboard('jane');
+//   const [nameInput, emailInput] = screen.getAllByRole('textbox');
+//   const button = screen.getByRole('button');
 
-    user.click(emailInput);
-    user.keyboard('jane@jane.com');
+//   // eslint-disable-next-line testing-library/no-unnecessary-act
+//   act(() => {
+//     user.click(nameInput);
+//     user.keyboard('jane');
 
-    user.click(button);
-  });
+//     user.click(emailInput);
+//     user.keyboard('jane@jane.com');
 
-  expect(argList).toHaveLength(1);
-  expect(argList[0][0]).toEqual({ name: 'jane', email: 'jane@jane.com' });
-});
+//     user.click(button);
+//   });
+
+//   expect(argList).toHaveLength(1);
+//   expect(argList[0][0]).toEqual({ name: 'jane', email: 'jane@jane.com' });
+// });
