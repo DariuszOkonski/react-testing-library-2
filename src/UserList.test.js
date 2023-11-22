71f8a5c964901625e667de999de0cf3375163baf
Dariusz Okoński <darek200180@gmail.com>
2023-11-22 20:43:59 +0100

    get element by container

--- src/UserList.test.js
+++ src/UserList.test.js
@@ -58,4 +58,17 @@ test('render one row per user (data-testid="users" and within)', () => {
   expect(rows).toHaveLength(2);
 });
 
+test('render one row per user (by returning container from render)', () => {
+  const users = [
+    { name: 'jane', email: 'jane@jane.com' },
+    { name: 'sam', email: 'sam@sam.com' },
+  ];
+  const { container } = render(<UserList users={users} />);
+
+  // eslint-disable-next-line
+  const rows = container.querySelectorAll('tbody tr');
+
+  expect(rows).toHaveLength(2);
+});
+
 test('render the email and name of each user', () => {});
