e4325a94098b675a5f6d63ca800893dd890d11c2
Dariusz Okoński <darek200180@gmail.com>
2023-12-05 22:25:36 +0100

    add ColorList

---
+++ src/components/ColorList.test.js
@@ -0,0 +1,20 @@
+import { render, screen } from '@testing-library/react';
+import ColorList from './ColorList';
+
+test('getBy, queryBy, findBy finding 0 elements', async () => {
+  render(<ColorList />);
+
+  expect(() => screen.getByRole('textbox')).toThrow();
+
+  expect(screen.queryByRole('textbox')).toEqual(null);
+
+  let errorThrown = false;
+
+  try {
+    await screen.findByRole('textbox');
+  } catch (error) {
+    errorThrown = true;
+  }
+
+  expect(errorThrown).toEqual(true);
+});
